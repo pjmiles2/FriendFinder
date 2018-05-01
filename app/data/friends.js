4f8a56b4e30bdefdd28f5475521bd2da06b741e9
Patrick Miles <patrick.miles@gmail.com>
2018-05-01 16:40:35 -0700

    working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,9 @@
+
+
 var friendArray = [
     {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      name: "Patrick",
+      photo: "https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/12295376_10105702880977620_4769927428327997066_n.jpg?_nc_cat=0&oh=8fe04498a70b17f4b96b4863318c6227&oe=5B63B9E2",
       scores: [
         5,
         1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,51 @@
 
-var friends = require("../data/friends");
-
-
+var friends = require('../data/friends.js');
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+    //Tables API route
+    app.get("/api/friends", function(req, res) {
+        return res.json(friends);
+    });
 
+    //Create new friend
+    app.post("/api/friends", function(req, res) {
 
-  app.post("/api/friends", function(req, res) {
-  
+        //Empty array to push matched friend
+        var bestFriend = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
 
+        var userData = req.body;
+        var userScores = userData.scores;
+        var totalDifference = 0;
 
-  });
+        //Loop through friends object and compare
+        for (var i = 0; i < friends.length; i++) {
 
+            totalDifference = 0;
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friends = [];
+            //Loop through the scores of each friend
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                //calculating the difference between each score and sum them into totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
-    console.log(friends);
-  });
-};
+                //Find best friend match
+                if (totalDifference <= bestFriend.friendDifference) {
+
+                    bestFriend.name = friends[i].name;
+                    bestFriend.photo = friends[i].photo;
+                    bestFriend.friendDifference = totalDifference;
+                    
+                }
+            }
+        }
+
+        //Pushing new friend to friends API
+        friends.push(userData);
+        res.json(bestFriend);
+    });
+
+}
