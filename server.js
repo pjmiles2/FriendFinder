c8ca3065dd1147d9568c092050a2e51608c3f854
Patrick Miles <patrick.miles@gmail.com>
2018-04-30 21:01:15 -0700

    questions added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
@@ -10,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
